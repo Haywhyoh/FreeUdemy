e9f99c4f99785f32f32c26c3e421046f23c3f67c
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-26 16:27:49 +0100

    house cleaning the project

--- front_end/dist/dashborad.py
+++ front_end/dist/dashborad.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template, url_for
 import requests
+import json
 
 app = Flask(__name__)
-headers = {
-  "Accept": "application/json, text/plain, */*",
-  "Authorization": "Basic TDgwQzhqdm1lcEtraVVrNDdDSjEzcklhMGVWMEdERDVoVXJOV0xIeTpOZEVRWUhNbVBhUVdXMm9DRE5hZ3JMTUxhWXRZZEdZcU1GU3lXNDRldUYxZUVmRmNidklnc3BqZDF1cUt1WTR1cDk1ME9YaERYZnM5Q3JNbjhwbENIeVNxVkVFSVZodGp6MlZVVXB6MFh6WXpBZkx0b3dOb2FYQ1A1SGtqQm9hUw==",
-  "Content-Type": "application/json"
-}
-response = requests.get("https://www.udemy.com/api-2.0/courses/?page=1&page_size=10", headers=headers).json()
-courses = response["results"]
+# headers = {
+#   "Accept": "application/json, text/plain, */*",
+#   "Authorization": "Basic TDgwQzhqdm1lcEtraVVrNDdDSjEzcklhMGVWMEdERDVoVXJOV0xIeTpOZEVRWUhNbVBhUVdXMm9DRE5hZ3JMTUxhWXRZZEdZcU1GU3lXNDRldUYxZUVmRmNidklnc3BqZDF1cUt1WTR1cDk1ME9YaERYZnM5Q3JNbjhwbENIeVNxVkVFSVZodGp6MlZVVXB6MFh6WXpBZkx0b3dOb2FYQ1A1SGtqQm9hUw==",
+#   "Content-Type": "application/json"
+# }
+# response = requests.get("https://www.udemy.com/api-2.0/courses/?page=1&page_size=10", headers=headers).json()
+# courses = response["results"]
 
+with open("/Users/macbook/Documents/Personal_Projects/Web-Dev /FreeUdemy/data.json") as file:
+    response = json.load(file)
+
+courses = response["results"]
+print (courses)
 @app.route('/overview', strict_slashes=False)
 def dashboard():
     return render_template('overview.html')
