ce808a4fc45b36c842e30a6c8667d48220da15f2
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-02-09 10:19:41 +0100

    import the categories collection

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,9 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models.engine.db_storage import DBStorage
+from models.design import design as Design
+from models.development import development as Development
+from models.all_courses import courses as Courses
 
 storage = DBStorage()
 
@@ -13,11 +16,11 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def number_objects():
     """ Retrieves the number of each objects by type """
-    categories = ["Business", "Design", "Development", "Finance & Accounting",
+    categories = ["Courses", "Business", "Design", "Development", "Finance & Accounting",
                     "Health & Fitness", "IT & Software", "Lifestyle", 
                     "Marketing", "Music", "Office", "Productivity", "Personal Development",
                     "Photography & Video", "Teaching & Academics"]
-    names = ["business", "design", "development", "finance & accounting",
+    names = ["courses", "business", "design", "development", "finance & accounting",
                     "health & Fitness", "iT & software", "lifestyle", 
                     "marketing", "music", "office", "productivity", "personal development",
                     "photography & video", "teaching & academics"]
