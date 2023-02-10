4133774246de5dc2295abb109222cfa7b7503dd9
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-02-10 12:19:24 +0100

    made more import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,3 +5,8 @@ app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.all_courses import *
+from api.v1.views.design import *
+from api.v1.views.development import *
+from api.v1.views.finance import *
+from api.v1.views.health import *
+from api.v1.views.software import *
